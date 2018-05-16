a1ddb850a334c12cde403b12d93c689ad75591b3
puppitty <33668867+puppitty@users.noreply.github.com>
2018-05-16 15:49:40 -0400

    Partially working button clicks

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -46,84 +46,120 @@ $(document).ready(function () {
     }
   ];
 
-  var qCount = (triviaQs.length);
+  // var qCount = (triviaQs.length);
   // var startGame;
 
   // if Start is clicked, start the game, Hide the button and go Ask the questions
   $("#start").click(function () {
-      $("#start").hide();
-      // $(".result").remove();
-      // $("div").html("");
-      // startGame = new $(window).startGame();
-      askQuestion();
-      timer();
+    $("#start").hide();
+    // $(".result").remove();
+    // $("div").html("");
+    // startGame = new $(window).startGame();
+    askQuestion();
+    timer();
+  });
 
-      function timer() {
-        qTimer--;
-        if (qTimer <= 0) {
-          setTimeout(function () {
-            nextQ();
-          });
+
+
+  function askQuestion() {
+
+    // Display trivia question
+    if (qNumber < triviaQs.length) {
+      $("#timer").html("<h4>Time remaining: " + "00:" + qTimer + "secs</h4>");
+      $(".question").html(triviaQs[qNumber].question);
+      // console.log(triviaQs[qNumber].question);
+
+      // Display possible answers as buttons using same format as #start button
+
+      var choicesArr = triviaQs[qNumber].choices;
+      var buttonsArr = [];
+
+      // console.log(choicesArr);
+      // console.log(qNumber);
+
+      for (var i = 0; i < choicesArr.length; i++) {
+        // var choice1 = choicesArr[i];
+        var button = $("<button>");
+        button.text(choicesArr[i]);
+        console.log(button);
+        button.attr("button-id", i);
+        console.log(i);
+        $(".choices-div").append(button);
+        console.log(button);
+      }
+
+      window.triviaCounter = setInterval(qTimer, 1000);
+      $(".choices-div").on("click", 'button', function (e) {
+        var userPick = $(this).data("id");
+        var correctAns = triviaQs[qNumber].correct;
+        if (userPick !== correctAns) {
+          $(".choices-div").text("Wrong answer! The correct answer was: " + triviaQs[correctAns].choices);
+          totalWrong++;
         } else {
-          $("#timer").html("<h4>Time remaining: " + "00:" + qTimer + "secs</h4>");
+          $(".choices-div").text("Correct!!! The correct answer was: " + triviaQs[correctAns].choices);
+          totalCorect++;
         }
-      };
+        nextQ();
+      });
 
-      function askQuestion() {
-
-        // Display trivia question
-        if (qNumber < triviaQs.length) {
-          $("#timer").html("<h4>Time remaining: " + "00:" + qTimer + "secs</h4>");
-          $(".question").html(triviaQs[qNumber].question);
-          // console.log(triviaQs[qNumber].question);
-
-          // Display possible answers as buttons using same format as #start button
-
-          var choicesArr = triviaQs[qNumber].choices;
-          var buttonsArr = [];
-
-          // console.log(choicesArr);
-          // console.log(qNumber);
-
-          for (var i = 0; i < choicesArr.length; i++) {
-            // var choice1 = choicesArr[i];
-            var button = $("<button>");
-            button.text(choicesArr[i]);
-            button.attr(".choices-div, i");
-            $(".choices-div").append(button);
-            console.log(button);
-          }
-        }
-        $(document.body).on("click", button, function () {
-
-          // Get the number of the button from its data attribute and hold in a variable called  toDoNumber.
-          console.log(button);
-          var userPick = $(this).attr("choices_div");
-
-          // Select and Remove the specific <p> element that previously held the to do item number.
-          // $("#item-" + toDoNumber).remove();
-        });
-      }
-
-      function nextQ() {
-        qNumber++
-        clearInterval();
-      }
-      // button.attr(".data-id", );
-      // $("#choicediv").append(button);
-      // console.log(".data-id");
-
-      // window.triviaCounter = setInterval(qTimer, 1000);
-      // Start timer
-      // Add Else for end of game to show totals
+    } else {
+      // end of questions, provide totals and offer to restart the game
     }
 
+    // $(document).ready(function () {
+    //   $("<button>").click(function () {
+    //     $(this).hide();
+    // $(document.body).on("click", button, function () {
 
-    //Listen for click on answer or check to see if timer is 0. If timer 0 then go to display answer
+    // Get the number of the button from its data attribute and hold in a variable called  toDoNumber.
+    // console.log(button);
+    // var userPick = $(this).attr("choices_div");
+    // console.log("This is the User's choice: " + userPick)
 
-    // check answer to see if it is correct and display appropriate message, correct answer and image (Add timer), add to counter, when timer is 0, go to Next Question
+    // Select and Remove the specific <p> element that previously held the to do item number.
+    // $("#item-" + toDoNumber).remove();
+  }
+  //   })
+  // }
 
-    // end game function
+  function timer() {
+    qTimer--;
+    if (qTimer <= 0) {
+      setTimeout(function () {
+        nextQ();
+      });
+    } else {
+      $("#timer").html("<h4>Time remaining: " + "00:" + qTimer + "secs</h4>");
+    }
+  };
 
-  );
-})
+  function nextQ() {
+    qNumber++
+    clearInterval(window.triviaCounter);
+    qTimer = 30;
+    $('#timer').html("");
+    setTimeout(function () {
+      cleanUp();
+      askQuestion();
+    }, 1000)
+  };
+
+  function cleanUp() {
+    console.log("What needs to be cleaned?");
+  }
+  // button.attr(".data-id", );
+  // $("#choicediv").append(button);
+  // console.log(".data-id");
+
+  // window.triviaCounter = setInterval(qTimer, 1000);
+  // Start timer
+  // Add Else for end of game to show totals
+});
+
+
+
+//Listen for click on answer or check to see if timer is 0. If timer 0 then go to display answer
+
+// check answer to see if it is correct and display appropriate message, correct answer and image (Add timer), add to counter, when timer is 0, go to Next Question
+
+// end game function
